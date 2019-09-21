5a2ae8057c2aa299623c84e84ecbacfa3869cd7e
Michael Georg Hansen <mike@mghansen.de>
2019-09-21 22:34:58 +0200

    Add new dummy project to install all gradle dependencies in the base docker image.

---
+++ docker/dummy-project/main.cpp
@@ -0,0 +1,31 @@
+/** ===========================================================
+ * @file
+ *
+ * @date   2019-09-21
+ * @brief  Main file for dummy project
+ *
+ * @author Copyright (C) 2019 by Michael G. Hansen
+ *         <a href="mailto:mike at mghansen dot de">mike at mghansen dot de</a>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include <QApplication>
+
+int main(int argc, char* argv[])
+{
+    QApplication app(argc, argv);
+
+    // TODO: print something?
+
+    return 0;
+}
